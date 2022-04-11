c07173ef37ca71cc4d4ecc6be72bfa3d620c1836
“7painapo7” <53612675+7painapo7@users.noreply.github.com>
2022-04-11 11:55:47 +0000

    [Finish]chapter14

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@
 #
 
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
 
   devise_for :users
   root to: "homes#top"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get 'homes/about', to: 'homes#about', as: :about
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
+
 end

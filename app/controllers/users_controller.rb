deed2c21b629cc3154456f434bc44ee790cf8aab
“7painapo7” <53612675+7painapo7@users.noreply.github.com>
2022-04-11 12:47:28 +0000

    [Finish]chapter16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
   def show
     @user = current_user
     # binding.pry
@@ -7,5 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end

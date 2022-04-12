181ba36c9dfeebb73308c5f26a05a6cecf08420e
“7painapo7” <53612675+7painapo7@users.noreply.github.com>
2022-04-12 11:41:34 +0000

    [Finish]chapter18

---
+++ test/models/post_comment_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: post_comments
+#
+#  id            :integer          not null, primary key
+#  comment       :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  post_image_id :integer
+#  user_id       :integer
+#
+require "test_helper"
+
+class PostCommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
